00b0d33e8e8b7239147bd895bf12bc83eac1db50
shloka pandya <shlokapan10@gmail.com>
2020-02-04 17:43:37 +0530

    please

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Instagram
+                    LARAVEL
                 </div>
 
                 <div class="links">
